d2a7087d22380c7c55899ba0a633c5329494ce55
Jordan Rhodes <jsrhodes15@gmail.com>
2018-03-20 18:33:26 -0400

    Rearrange file structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Home from './modules/homePage';
+import HomePage from './containers/HomePage';
 
 class App extends Component {
   render() {
     return (
       <Router>
       <div>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={HomePage} />
       </div>
       </Router>
     );
--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -12,10 +12,10 @@ class HomePage extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Music Book</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          
         </p>
       </div>
     );
