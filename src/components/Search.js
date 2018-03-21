ff570f93c1c2b64236644e57369f887e521d57be
Jordan Rhodes <jsrhodes15@gmail.com>
2018-03-20 23:18:08 -0400

    Add styling

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,20 +2,25 @@ import React from 'react';
 
 
 const SearchPage = ({ handleSubmit, setSearchValue, value }) => (
+  <div className="search-form">
   <form onSubmit={handleSubmit}>
-  <label>Artist</label>
-    <input
-      className="form-control"
-      name="artist"
-      type="text"
-      value={value}
-      onChange={(e) => setSearchValue(e.target.value)}
-    />
+    <div className="form-group">
+      <label htmlFor="artistName">Artist Name</label>
+      <input
+        className="form-control"
+        id="artistName"
+        type="text"
+        value={value}
+        placeHolder="Enter the artist you are looking for"
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+    </div>
 
-    <button
-      type="submit"
-      className="btn btn-primary">Search</button>
+      <button
+        type="submit"
+        className="btn btn-primary">Search</button>
   </form>
+  </div>
 )
 
 export default SearchPage;
