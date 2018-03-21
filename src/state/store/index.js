9d80f2e1a7460f18b8703bf4f37e742a0d2bba45
Jordan Rhodes <jsrhodes15@gmail.com>
2018-03-21 09:01:17 -0400

    Set up for easter egg

--- src/state/store/index.js
+++ src/state/store/index.js
@@ -1,13 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
-import loggerMiddleware from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState = {}) {
+  let middleware = [thunkMiddleware];
+
+  if (process.env.NODE_ENV !== 'production') {
+    const createLogger = require('redux-logger').createLogger;
+    const loggerMiddleware = createLogger();
+    middleware = [ ...middleware, loggerMiddleware ];
+  }
+
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunkMiddleware, loggerMiddleware),
+    applyMiddleware(...middleware),
   )
 }
